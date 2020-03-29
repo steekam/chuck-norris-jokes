14d6e15fa8499a0f78e5b2cfc2f282d59dd81315
Kamau Wanyee <steekam@users.noreply.github.com>
2020-03-30 00:42:37 +0300

    Apply fixes from StyleCI (#1)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -5,16 +5,18 @@ namespace Steekam\ChuckNorrisJokes;
 class JokeFactory
 {
     protected array $jokes = [
-        "The dinosaurs looked at Chuck Norris the wrong way once ...ONCE.",
+        'The dinosaurs looked at Chuck Norris the wrong way once ...ONCE.',
         "Chuck Norris' tears cure cancer. Too bad he has never cried.",
-        "The chief export of Chuck Norris is pain.",
-        "Chuck Norris recently had the idea to sell his urine as a canned beverage. We know this beverage as Red Bull.",
-        "On the 7th day, God rested ... Chuck Norris took over."
+        'The chief export of Chuck Norris is pain.',
+        'Chuck Norris recently had the idea to sell his urine as a canned beverage. We know this beverage as Red Bull.',
+        'On the 7th day, God rested ... Chuck Norris took over.',
     ];
 
     public function __construct(array $jokes = null)
     {
-        if ($jokes) $this->jokes = $jokes;
+        if ($jokes) {
+            $this->jokes = $jokes;
+        }
     }
 
     public function getRandomJoke()
